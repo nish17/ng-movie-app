b207d1ebc4abb9e2cc9a3b68f913e51517ea1274
Nimesh Solanki <nimesh3536@gmail.com>
2020-01-13 15:54:27 +0530

    add searchBar component

---
+++ src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.css']
+})
+export class SearchBarComponent implements OnInit {
+  search;
+  searchForm;
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.searchForm = this.formBuilder.group({
+      search: ''
+    });
+  }
+  onSubmit() {
+    console.log(this.search);
+    this.searchForm.reset();
+  }
+
+}
